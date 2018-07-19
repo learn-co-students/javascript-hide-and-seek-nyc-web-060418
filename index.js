00f3b88d25696f9d650c7a1a15e2edc190c281ad
Sree <harsha6raju@yahoo.com>
2018-07-19 14:53:26 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested div.target')
+}
+
+function increaseRankBy(n) {
+
+  const rankedLists = document.querySelectorAll('.ranked-list li')
+  for (let i = 0; i < rankedLists.length; i++) {
+    debugger
+    rankedLists[i].innerHTML = parseInt(rankedLists[i].innerHTML) + n
+  }
+}
+
+function deepestChild() {
+  const array = document.querySelectorAll('#grand-node div')
+  return array[array.length - 1]
+}
