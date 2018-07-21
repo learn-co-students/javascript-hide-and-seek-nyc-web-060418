3636c5b9e793ae1dfe2150dc7763d75f2a76d3a5
Shing Wong <shing2487@gmail.com>
2018-07-21 15:36:24 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function deepestChild() {
+   let deepestNode = document.querySelectorAll('#grand-node div')
+
+   return deepestNode[deepestNode.length - 1]
+}
+
+function increaseRankBy(n){
+  let rankedList = document.querySelectorAll('.ranked-list')
+
+  let rankedListElements = rankedList.forEach(function(el, i, obj){
+    let li = el.querySelectorAll('li')
+    li.forEach(function(el){
+      let num = Number(el.innerHTML)
+      el.innerHTML = num + n;
+    })
+  })
+
+}
